a6f72a4b0fd8188d71e1bed0043aa2ad406abcbc
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-20 00:47:44 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  // projectStatuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      // 'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      // 'projectName': new FormControl(null, [Validators.required]),
+      'projectName': new FormControl(null, Validators.required, this.forbiddenNamesAsync),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(null)
+    });
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Test') {
+      return {'forbiddenName': true};
+    } else {
+      return null;
+    }
+  }
+
+  forbiddenNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'forbiddenName': true});
+        } else {
+          return resolve(null);
+        }
+      }, 2000);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    // this.myForm.reset();
+  }
+}
